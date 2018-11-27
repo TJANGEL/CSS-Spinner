013b6178ad989359d701267d2f2392c33487bc57
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-11-26 22:24:11 -0500

    added js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+document.querySelector('.main p').style.display = 'none';
+document.querySelector('.main').classList.add('spinner-3');
+
+setTimeout(() => {
+    document.querySelector('.main p').style.display = 'block';
+    document.querySelector('.main').classList.remove('spinner-3');
+
+}, 4000);
